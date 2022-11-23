b86602763763f6aa185d61b9a2adf5edcc9aa7c6
阿银 <739178270@qq.com>
2022-11-23 10:42:24 +0800

    fix 部署配置

--- vite.config.ts
+++ vite.config.ts
@@ -4,23 +4,29 @@ import vue from '@vitejs/plugin-vue';
 
 const path = require('path');
 
-export default defineConfig({
-  base: '/',
-  plugins: [vue()],
-  resolve: {
-    alias: [
-      {
-        find: '@',
-        replacement: path.resolve(__dirname, 'src'),
-      },
-    ],
-  },
-  css: {
-    preprocessorOptions: {
-      less: {
-        modifyVars: {},
-        javascriptEnabled: true,
+const config = isDev => {
+  return {
+    base: isDev ? '/' : '/free/',
+    plugins: [vue()],
+    resolve: {
+      alias: [
+        {
+          find: '@',
+          replacement: path.resolve(__dirname, 'src'),
+        },
+      ],
+    },
+    css: {
+      preprocessorOptions: {
+        less: {
+          modifyVars: {},
+          javascriptEnabled: true,
+        },
       },
     },
-  },
+  };
+};
+
+export default defineConfig(({ command }) => {
+  return config(command === 'serve');
 });
